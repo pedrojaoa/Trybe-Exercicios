6662cc18aeba0c80aa42a1008605a47dd13af238
pedrojaoa <joaopfacbc@gmail.com>
2022-08-23 19:16:55 -0300

    exercicio2.3 parte II

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia4-funcoes-e-objetos/exercicio2.3-4.4.js
@@ -0,0 +1,12 @@
+function indiceMenorValor(numeros) {
+    let indiceMenor = 0;
+    for (let indice in numeros) {
+        if (numeros[indiceMenor] > numeros[indice]) {
+        indiceMenor = indice;
+    }
+}
+return indiceMenor;
+
+}
+
+console.log(indiceMenorValor([2, 4, 6, 7, 10, 0, -3]));
